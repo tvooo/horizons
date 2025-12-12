4c30468ab00b119f50fa3ce705845f20044a6792
Tim von Oldenburg <tim@risecalendar.com>
2025-12-12 12:19:29 +0100

    Linting

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+// biome-ignore lint/style/noNonNullAssertion: We know this ID exists in the HTML.
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
