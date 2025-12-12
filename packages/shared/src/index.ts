7ade6bc1d6f7fe79acaf97178c04f8b44ba7c62b
Tim von Oldenburg <tim@risecalendar.com>
2025-12-12 12:14:23 +0100

    Formatting

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,10 +1,10 @@
 export interface User {
-  id: string;
-  email: string;
-  name: string;
+  id: string
+  email: string
+  name: string
 }
 
 export interface ApiResponse<T> {
-  data: T;
-  error?: string;
+  data: T
+  error?: string
 }
