f25c4fc083cef22e3f2bac3f71aed95ee97ed865
Tim von Oldenburg <tim@risecalendar.com>
2025-12-28 11:11:45 +0100

    Light UI improvements

---
+++ apps/frontend/src/components/sidebar/SidebarNavItem.tsx
@@ -0,0 +1,31 @@
+import clsx from 'clsx'
+import type { LucideIcon } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
+import { twMerge } from 'tailwind-merge'
+
+interface SidebarNavItemProps {
+  href: string
+  icon: LucideIcon
+  name: string
+}
+
+export function SidebarNavItem({ href, icon: Icon, name }: SidebarNavItemProps) {
+  return (
+    <NavLink
+      to={href}
+      className={({ isActive }) =>
+        twMerge(
+          clsx(
+            'flex cursor-pointer items-center gap-2 rounded px-2 py-1.5 text-gray-700 text-sm hover:bg-gray-100',
+            {
+              'bg-neutral-light text-gray-900': isActive,
+            },
+          ),
+        )
+      }
+    >
+      <Icon size={16} className="shrink-0 text-gray-500" />
+      <span className="flex-1">{name}</span>
+    </NavLink>
+  )
+}
